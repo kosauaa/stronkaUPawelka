02c5e68d4650cb5eedbc66cc82a672f2e1180fb7
kosauaa <henryk.kosala@szkola.salezjanie.com>
2021-11-18 12:02:51 +0100

    hi

---
+++ przesylanie.php
@@ -0,0 +1,22 @@
+<?php
+include "db_connection.php";
+ 
+if(isset($_POST['submit']))
+{
+ $name = $_POST['name'];
+ $email = $_POST['email'];
+ $message = $_POST['message'];
+ 
+ $insert = mysqli_query($db, "INSERT INTO `wiadomosci`(`imie`, `email`, `wiadomosc`) VALUES ('$name', '$email', '$message')");
+ 
+ if(!$insert)
+ {
+ echo mysqli_error();
+ }
+ else{
+ echo "Twoja wiadomość została wysłana";
+ }
+ 
+ mysqli_close($db);
+}
+?>
